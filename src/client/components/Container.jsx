fbd74b71ef84e7a28050f997371439e9b83f4299
GustavoFLuz <gu.luz400@gmail.com>
2023-04-12 16:51:57 -0300

    Criação Tela Home

--- src/client/components/Container.jsx
+++ src/client/components/Container.jsx
@@ -5,7 +5,7 @@ import { Container as MuiContainer } from "@mui/material"
 export const Container = () => {
     return (<>
         <BarraNavegacao>
-            <MuiContainer maxWidth="xl" sx={{border: '1px solid red', mt: 3}}>
+            <MuiContainer maxWidth="xl" sx={{mt: 3}}>
                 <Outlet />
             </MuiContainer>
         </BarraNavegacao>
