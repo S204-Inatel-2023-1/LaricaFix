27bceded12e5738d0f3fd6010ab1b60928bc7bbe
GustavoFLuz <gu.luz400@gmail.com>
2023-05-04 15:57:31 -0300

    Criação do mock e api de receitas

--- src/server/controller/Receitas/buscarReceitas.js
+++ src/server/controller/Receitas/buscarReceitas.js
@@ -1,3 +1,21 @@
 export default async function (req, res) {
-  
+  const nome = req.query.nome || '';
+  const ingredientes = req.query.ingredientes || '';
+  const dietas = req.query.dietas || '';
+  const intolerancias = req.query.intolerancias || '';
+
+  const requestUrl = new URL("https://api.spoonacular.com/recipes/complexSearch?apiKey=" + process.env.SPOONACULAR_KEY)
+
+  if (nome.length)
+    requestUrl.searchParams.append('query', nome);
+  if (ingredientes.length)
+    url.searchParams.append('includeIngredients', ingredientes);
+  if (dietas.length)
+    requestUrl.searchParams.append('diet', dietas);
+  if (intolerancias.length)
+    requestUrl.searchParams.append('intolerances', intolerancias);
+
+  fetch(requestUrl.href).then(response => response.json()).then(data => {
+    res.status(200).json(data)
+  })
 }
--- src/server/controller/Receitas/buscarReceitasPorID.js
+++ src/server/controller/Receitas/buscarReceitasPorID.js
@@ -1,3 +1,9 @@
 export default async function (req, res) {
-  
+  const idPrato = req.query.id
+
+  const requestUrl = new URL("https://api.spoonacular.com/recipes/" + idPrato + "/information?apiKey=" + process.env.SPOONACULAR_KEY + "&includeNutrition=true")
+
+  fetch(requestUrl.href).then(response => response.json()).then(data => {
+    res.status(200).json(data)
+  })
 }
