4c0a540791c08bb6385f86c645de413d9355f5ed
Samuel Alves Chagas <samuelchagasalves@gmail.com>
2023-05-25 14:42:07 -0300

    Possibilitando envio de apiKey e paginacao

--- src/server/controller/Receitas/buscarReceitas.js
+++ src/server/controller/Receitas/buscarReceitas.js
@@ -7,8 +7,10 @@ export default async function (req, res) {
     const ingredientes = req.query.ingredientes || '';
     const dietas = req.query.dietas || '';
     const intolerancias = req.query.intolerancias || '';
+    const pag = req.query.pag || '';
+    const apiKey = req.query.apiKey=='undefined' ? '': req.query.apiKey;
 
-    let url = "https://api.spoonacular.com/recipes/complexSearch?apiKey=" + process.env.SPOONACULAR_KEY
+    let url = "https://api.spoonacular.com/recipes/complexSearch?"
     if (nome.length)
       url = url.concat(`&query=${nome}`);
     if (ingredientes.length)
@@ -17,6 +19,13 @@ export default async function (req, res) {
       url = url.concat(`&diet=${dietas}`);
     if (intolerancias.length)
       url = url.concat(`&intolerances=${intolerancias}`);
+    if (pag.length)
+        url = url.concat(`&offset=${pag}`);
+    if (apiKey.length)
+      url = url.concat(`&apiKey=${apiKey}`);
+
+    url = url.concat(`&number=25`);
+
     const response = await axios.get(url);
     res.set("x-api-quota-request", response.headers["x-api-quota-request"]);
     res.set("x-api-quota-used", response.headers["x-api-quota-used"]);
