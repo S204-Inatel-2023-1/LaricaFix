1536a1bb29a9dbf87498ef8cd0792de19e1629d4
vonot16 <rafaelfeliperm@gmail.com>
2023-05-30 22:20:53 -0300

    Card de receita refeito, design melhorado

--- src/client/pages/Receita.jsx
+++ src/client/pages/Receita.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react"
 import { useLoaderData } from "react-router-dom";
 
 // Material components
-import { Card, CardContent, CardHeader, CardMedia, Chip, Divider, Grid, List, ListItemText, Typography } from "@mui/material"
+import { Card, CardContent, CardHeader, CardMedia, Chip, Divider, Grid, List, ListItemText, Paper, Typography } from "@mui/material"
 
 //Material Icons
 import TimerIcon from '@mui/icons-material/Timer';
@@ -17,7 +17,7 @@ import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 export const Receita = () => {
 
     const recipe = useLoaderData()
-    
+
     const [favorite, setFavorite] = useState(false)
 
     const toggleFavorite = () => {
@@ -32,7 +32,7 @@ export const Receita = () => {
         <Grid container spacing={5} columnGap={5}>
             <Grid item xs={4}>
                 <Card sx={{ backgroundColor: 'transparent' }}>
-                    <CardHeader title={recipe.title} />
+                    <CardHeader title={recipe.title} sx={{textAlign: 'center'}}/>
 
                     <CardMedia
                         component="img"
@@ -41,58 +41,58 @@ export const Receita = () => {
                         alt={recipe.title}
                     />
                     <CardContent>
-                        <Grid container rowSpacing={2}>
+                        <Grid container spacing={2}>
                             <Grid item xs={12}>
-                                <Chip icon={<TimerIcon />} label={`Tempo de Preparo ${recipe.readyInMinutes} minutos `} /> <br />
+                                <Chip icon={<TimerIcon />} label={`Tempo de Preparo ${recipe.readyInMinutes} minutos `} sx={{ width: '100%' }} />
                             </Grid>
                             <Grid item xs={12}>
-                                <Chip icon={<RestaurantMenuIcon />} label={`Serve ${recipe.servings} pessoas`} /><br />
+                                <Chip icon={<RestaurantMenuIcon />} label={`Serve ${recipe.servings} pessoas`} sx={{ width: '100%' }} />
                             </Grid>
                             <Grid item xs={6}>
-                                {recipe.vegetarian ? <Chip icon={<DoneIcon />} label={`Vegetariano`} color="success" /> : <Chip icon={<ClearIcon />} label={`Vegetariano`} color="error" />}
-                            </Grid>
-
-                            <Grid item sx={{ display: 'flex', justifyContent: 'flex-end' }} xs={6}>
-                                {recipe.dairyFree ? <Chip icon={<DoneIcon />} label={`Sem Lactose`} color="success" /> : <Chip icon={<ClearIcon />} label={`Sem Lactose`} color="error" />}
+                                {recipe.vegetarian ? <Chip icon={<DoneIcon />} label={`Vegetariano`} color="success" sx={{ width: '100%' }}/> : <Chip icon={<ClearIcon />} label={`Vegetariano`} color="error" sx={{ width: '100%' }}/>}
                             </Grid>
                             <Grid item xs={6}>
-                                {recipe.vegan ? <Chip icon={<DoneIcon />} label={`Vegano`} color="success" /> : <Chip icon={<ClearIcon />} label={`Vegano`} color="error" />} <br />
-                            </Grid>
-                            <Grid item sx={{ display: 'flex', justifyContent: 'flex-end' }} xs={6}>
-                                {recipe.glutenFree ? <Chip icon={<DoneIcon />} label={`Sem Glutem`} color="success" /> : <Chip icon={<ClearIcon />} label={`Sem Glutem`} color="error" />}
+                                {recipe.dairyFree ? <Chip icon={<DoneIcon />} label={`Sem Lactose`} color="success" sx={{ width: '100%' }}/> : <Chip icon={<ClearIcon />} label={`Sem Lactose`} color="error" sx={{ width: '100%' }}/>}
                             </Grid>
 
-                            <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
-                                <Chip
-                                    icon={favorite ? <FavoriteIcon /> : <FavoriteBorderIcon />}
-                                    onClick={toggleFavorite} label={favorite ? "Adicionado aos favoritos" : "Adicionar aos favoritos"}
-                                    color="primary" />
+                            <Grid item xs={6}>
+                            {recipe.vegan ? <Chip icon={<DoneIcon />} label={`Vegano`} color="success" sx={{ width: '100%' }}/> : <Chip icon={<ClearIcon />} label={`Vegano`} color="error" sx={{ width: '100%' }}/>}
                             </Grid>
+                            <Grid item xs={6}>
+                            {recipe.glutenFree ? <Chip icon={<DoneIcon />} label={`Sem Glutem`} color="success" sx={{ width: '100%' }}/> : <Chip icon={<ClearIcon />} label={`Sem Glutem`} color="error" sx={{ width: '100%' }}/>}
+                            </Grid>
+
+                            <Grid item xs={12}>
+                            <Chip
+                            icon={favorite ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+                            onClick={toggleFavorite} label={favorite ? "Adicionado aos favoritos" : "Adicionar aos favoritos"}
+                            color="primary" sx={{ width: '100%' }}/>
+                            </Grid>
+
                         </Grid>
+
 
 
                     </CardContent>
                 </Card>
             </Grid>
 
-            <Divider orientation="vertical" flexItem variant="middle" />
-
             <Grid item xs>
-                <Typography sx={{ mt: 1, mb: 2 }} variant="h6" component="div">
-                    Ingredientes
-                </Typography>
+                <Paper>
+                    <Typography sx={{ mt: 1, mb: 2 }} variant="h6" component="div">
+                        Ingredientes
+                    </Typography>
 
-                <List dense>
-                    {recipe.extendedIngredients.map((i) => (
-                        <ListItemText primary={i.aisle}
-                            secondary={i.unit ?
-                                <>{i.measures.metric.amount} {i.measures.metric.unitShort} </> :
-                                <>{i.measures.us.amount} {i.measures.us.unitShort}</>}>
-                        </ListItemText>))
-                    }
-                </List>
-
-
+                    <List dense>
+                        {recipe.extendedIngredients.map((i) => (
+                            <ListItemText primary={i.aisle}
+                                secondary={i.unit ?
+                                    <>{i.measures.metric.amount} {i.measures.metric.unitShort} </> :
+                                    <>{i.measures.us.amount} {i.measures.us.unitShort}</>}>
+                            </ListItemText>))
+                        }
+                    </List>
+                </Paper>
             </Grid>
 
             {recipe.analyzedInstructions && <Grid item xs={5}>
@@ -114,7 +114,7 @@ export const Receita = () => {
                             ))}
                         </List >
 
-                        <br/>
+                        <br />
                     </>
                 ))}
 
