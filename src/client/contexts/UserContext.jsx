0266f7303de20cff70833f1d1ea29cbde9f201b7
GustavoFLuz <gu.luz400@gmail.com>
2023-06-26 17:13:09 -0300

    Adicionado login ao usuário

--- src/client/contexts/UserContext.jsx
+++ src/client/contexts/UserContext.jsx
@@ -2,14 +2,48 @@ import React, { useEffect, createContext, useState } from 'react'
 
 export const UserContext = createContext({
     logged: null, setLogged: () => { },
+    user: null, setUser: () => { },
+    retornarUser: () => { },
+    cadastrarUser: () => { },
 })
 
 export const UserProvider = ({ children }) => {
     const [logged, setLogged] = useState(null)
+    const [user, setUser] = useState(null)
+
+    const cadastrarUser = async (nome, email, senha) => {
+        const response = await fetch("/api/usuario", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ nome, email, senha })
+        })
+        const data = await response.json()
+        if (data.error) {
+            alert(data.error)
+            return false
+        }
+        return data
+    }
+
+    const retornarUser = async (email, senha) => {
+        const response = await fetch(`/api/usuario?email=${email}&senha=${senha}`)
+        const data = await response.json()
+        if (data.error) {
+            alert(data.error)
+            return null
+        }
+        return data
+    }
+
+    useEffect(() => {
+        setLogged(Boolean(user))
+    }, [user])
 
     return (
         <UserContext.Provider value={{
             logged, setLogged,
+            user, setUser,
+            cadastrarUser, retornarUser
         }}>
             {children}
         </UserContext.Provider>
