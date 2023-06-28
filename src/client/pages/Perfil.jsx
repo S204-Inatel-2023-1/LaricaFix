11186eb79fce042f64ac51417facb6274f1a007f
vonot16 <rafaelfeliperm@gmail.com>
2023-06-28 02:35:40 -0300

    Tela perfil responsiva

--- src/client/pages/Perfil.jsx
+++ src/client/pages/Perfil.jsx
@@ -1,32 +1,35 @@
-import { Box, Avatar, Skeleton, Typography } from "@mui/material"
+import { Box, Avatar, Skeleton, Typography, Grid } from "@mui/material"
 import { useContext, useState } from "react"
 import EditIcon from '@mui/icons-material/Edit';
 import { UserContext } from "../contexts";
+import { fontWeight, height } from "@mui/system";
 
 export const Perfil = () => {
     const [imageLoaded, setImageLoaded] = useState(true)
     const UserContextData = useContext(UserContext)
     const user = UserContextData.user
     return (
-        <Box sx={{width:"60%", m: '3em auto', display:'flex', gap:"20px", alignItems:'center', backgroundColor: "background.paper", borderRadius: "30px", position:"relative"}}>
-            <Box sx={{position:"absolute", top:"20px", right:"20px", cursor:"pointer"}}>
-                <EditIcon sx={{ fontSize: 40 }} />
-            </Box>
-            <Box sx={{ width: "40%", aspectRatio: '1', p:8 }}>
-                {!imageLoaded ?
-                    <Skeleton variant="circular" sx={{ width: "100%", height: "100%" }} /> :
-                    <Avatar sx={{ width: "100%", height: "100%" }}></Avatar>
-                }
-            </Box>
-            <Box>
-                <Typography variant="h3" sx={{ fontWeight: "600", }}>
-                    {user.nome}
-                </Typography>
-                <Typography variant="h4" sx={{ fontWeight: "300", }}>
-                    {user.email}
-                </Typography>
+        <Grid container sx={{position:'absolute',margin:'auto', width:'80%', alignItems: 'center', backgroundColor: "background.paper", borderRadius: "30px", position: "relative", paddingBottom:'10px' }}>
+            <Grid item xs={12} md={6}>
+                <Box sx={{ width: "100%", aspectRatio: '1', p: 8, display:"flex", alignItems:'center'}}>
+                    {!imageLoaded ?
+                        <Skeleton variant="circular" /> :
+                        <Avatar sx={{ width: "100%", height: "100%" }}></Avatar>
+                    }
+                </Box>
+            </Grid>
 
-            </Box>
-        </Box>
+            <Grid item xs={12} md={6}>
+                <Box>
+                    <Typography variant="h3" sx={{textAlign:'center', fontWeight:500, verticalAlign: 'middle'}}>
+                        {user.nome}
+                    </Typography>
+                    <Typography sx={{textAlign:'center', fontWeight:300}}> 
+                        {user.email}
+                    </Typography>
+
+                </Box>
+            </Grid>
+        </Grid>
     )
 }
